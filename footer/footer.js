394848bc7319e618d36ab0ecfbdc95d2d17dbc27
KingGoose20 <41673777+KingGoose20@users.noreply.github.com>
2025-11-23 14:38:19 +1000

    Footer Updates

---
+++ footer/footer.js
@@ -0,0 +1,8 @@
+const scriptPath = document.currentScript.src;
+const footerDir = scriptPath.substring(0, scriptPath.lastIndexOf('/'));
+
+fetch(`${footerDir}/footer.html`)
+  .then(r => r.text())
+  .then(html => {
+    document.getElementById('footer').innerHTML = html;
+  });
